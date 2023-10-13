633808b3a6ff420a271187421a1186190d47b96f
Adriano Junior <adrianojunior.dev97@gmail.com>
2023-10-13 16:56:36 -0300

    feat: implement component and add icon

---
+++ src/components/RoundIconButton/TelegramIcon.tsx
@@ -0,0 +1,30 @@
+const TelegramIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      shapeRendering="geometricPrecision"
+      textRendering="geometricPrecision"
+      imageRendering="optimizeQuality"
+      fillRule="evenodd"
+      clipRule="evenodd"
+      viewBox="0 0 439 512.167"
+    >
+      <g fillRule="nonzero">
+        <path
+          fill="transparent"
+          d="M402.415 237.792l-73.165 73.166h-73.167l-64.021 64.021v-64.021H109.75V36.585h292.665z"
+        />
+        <path
+          fill="#fff"
+          d="M402.415 237.792l-73.165 73.166h-73.167l-64.021 64.021v-64.021H109.75V36.585h292.665v201.207zM91.458 0L0 91.457v329.251h109.75v91.458l91.458-91.458h73.167L439 256.083V0H91.458z"
+        />
+        <path
+          fill="#fff"
+          d="M310.958 210.354h36.584v-109.75h-36.584zM210.354 210.354h36.583v-109.75h-36.583z"
+        />
+      </g>
+    </svg>
+  )
+}
+
+export default TelegramIcon
