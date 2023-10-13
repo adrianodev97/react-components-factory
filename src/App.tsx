ce16f7cffed988e3c6cfdcc655803607a92e152e
Adriano Junior <adrianojunior.dev97@gmail.com>
2023-10-13 02:47:52 -0300

    add all styles dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import styled from 'styled-components'
+
+const Main = styled.main`
+  background: #cbcbcb;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
 function App() {
-  return <></>
+  return (
+    <>
+      <Main></Main>
+    </>
+  )
 }
 
 export default App
